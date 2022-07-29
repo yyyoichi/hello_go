50e252ae6eb3d81c5f29e7af12fd5cff8c5070f0
yyyoichi <origamiichiro@gmail.com>
2022-07-29 23:07:17 +0000

    remove

--- hello.go
+++ hello.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"strconv"
 )
@@ -21,32 +20,5 @@ func s2i(s string) int {
 	return v
 }
 func main() {
-	l1 := nextLine()
-	n := s2i(l1)
-	board := make([]string, n)
-	for i := 0; i < n; i++ {
-		board[i] = nextLine()
-	}
 
-	for i := 0; i < n; i++ {
-		for j := i + 1; j < n; j++ {
-			if i == j {
-				continue
-			}
-			a := board[i][j : j+1]
-			b := board[j][i : i+1]
-			if a == "W" && b == "L" {
-				continue
-			}
-			if a == "L" && b == "W" {
-				continue
-			}
-			if a == "D" && b == "D" {
-				continue
-			}
-			fmt.Println("incorrect")
-			return
-		}
-	}
-	fmt.Println("correct")
 }
