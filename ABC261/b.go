7a2c7876c181150d8ee2c672af78fb7f302bb91c
yyyoichi <origamiichiro@gmail.com>
2022-07-31 06:48:42 +0000

    abc261c

--- ABC261/b.go
+++ ABC261/b.go
@@ -1,26 +1,13 @@
 package abc261
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+	// "bufio"
+	// "os"
+	// "strconv"
 )
 
-var sc = bufio.NewScanner(os.Stdin)
-
-func nextLine() string {
-	sc.Scan()
-	return sc.Text()
-}
-func s2i(s string) int {
-	v, ok := strconv.Atoi(s)
-	if ok != nil {
-		panic("Faild : " + s + " can't convert to int")
-	}
-	return v
-}
-func main() {
+func b() {
 	l1 := nextLine()
 	n := s2i(l1)
 	board := make([]string, n)
